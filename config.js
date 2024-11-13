29199c122f83407761a90419e46b8d634e70aacd
Navinofc44 <Pahasarasanujja@gmail.com>
2024-11-13 22:50:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "OQw22CRZ#7gs1XWLpX2dCL4VS40J0fLMasjwpgUKH5XAP9FkfWPI",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vJhwvYf/Rf-Y1-Cy4-X9914.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🧑‍💻🔰 Hi QUEEN-CHOOTY-NELUMI-MD Is Online Now 💻\n*💻 Owner* - QUEEN-CHOOTY-NELUMI-MD\n\n*💻 Owner Number* -94757286833",
 SUDO_NB: process.env.SUDO_NB || "94757286833",
