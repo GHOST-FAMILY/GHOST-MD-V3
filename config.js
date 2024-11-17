e4ab027608db6b9351a32bf169eb388999e8f1a2
Navinofc44 <Pahasarasanujja@gmail.com>
2024-11-17 22:08:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vNpnhQoI#YAgIElf8QSXQLz4MLUjk_dVFk0asFnOUgI1avt34D18",
-MONGODB: process.env.MONGODB || "mongodb+srv://Saka:Saka@cluster0.jadwggo.mongodb.net/?retryWrites=true&w=majority",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vJhwvYf/Rf-Y1-Cy4-X9914.jpg"
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vJhwvYf/Rf-Y1-Cy4-X9914.jpg",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
+MODE: process.env.MODE || "public", //private or public
+AUTO_VOICE:"true", //true or false
+ANTI_BAD:process.env.ANTI_BAD || "true", //true or false
+ANTI_DEL:process.env.ANTI_DEL || "true", // true or false
+ANTI_BOT:process.env.ANTI_BOT || "true", // true or false
+ANTI_LINK:process.env.ANTI_LINK || "true" // true or false
 };
-
