29bc2c59a52c340bdbb96b09a172dc773a6a01c4
Navinofc44 <Pahasarasanujja@gmail.com>
2024-11-17 22:44:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,5 @@ SESSION_ID: process.env.SESSION_ID || "bYJUxICb#kc-xWgfGzABmalp-KRkPNOWAe0mg26Bt
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vJhwvYf/Rf-Y1-Cy4-X9914.jpg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
-AUTO_VOICE:"true", //true or false
-ANTI_BAD:process.env.ANTI_BAD || "true", //true or false
-ANTI_DEL:process.env.ANTI_DEL || "true", // true or false
-ANTI_BOT:process.env.ANTI_BOT || "true", // true or false
-ANTI_LINK:process.env.ANTI_LINK || "true" // true or false
+AUTO_VOICE:"true" //true or false
 };
