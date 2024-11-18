b40c73306789e8684f6551b21679aeb3dde0fb75
Navinofc44 <Pahasarasanujja@gmail.com>
2024-11-18 23:24:44 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,45 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "menu",
+    desc: "To get the menu.",
+    react: "📜",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let desc = `*👋 Hello ${pushname}*
+
+*╭─「 ᴄᴏᴍᴍᴀɴᴅ ᴘᴀɴᴇʟ 」*
+*│◈ ʀᴜɴᴛɪᴍᴇ :* ${runtime(process.uptime())}
+*│◈ ʀᴀᴍ ᴜꜱᴀɢᴇ :* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*╰──────────●●►*
+*╭──────────●●►*
+*│⛵ LIST MENU*
+*│   ───────*
+*│ 1   OWNER*
+*│ 2   CONVERT*
+*│ 3   AI*
+*│ 4   SEARCH*
+*│ 5   DOWNLOAD*
+*│ 6   FUN*
+*│ 7   MAIN*
+*│ 8   GROUP*
+*│ 9   OTHER*
+*╰───────────●●►*
+
+*🔢 Reply the Number you want to select*
+
+> *Qᴜᴇᴇɴ ᴄʜᴏᴏᴛʏ ɴᴇʟᴜᴍɪ ᴍᴅ*`
+
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/vJhwvYf/Rf-Y1-Cy4-X9914.jpg`},caption:madeMenu},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`Error`)
+}
+})
