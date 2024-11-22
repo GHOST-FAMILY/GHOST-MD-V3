266dbdafca56856d221d3562f14c4155bf087b0a
GHOST-MD <tharindudenuwan65@gmail.com>
2024-11-22 05:54:53 +0530

    Create song&video.js

---
+++ plugins/song&video.js
@@ -0,0 +1,132 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    react: "💿",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*WAIT PLEASE...*")
+const search = await yts(q)
+const data = search.videos[0]
+const url = data.url
+
+let desc = `*◆ GHOST-MD SONG DOWNLOADING.....◆*
+
+| ➤ TITLE - ${data.title}
+
+| ➤ VIEWS - ${data.views}
+
+| ➤ DESCRIPTION - ${data.description}
+
+| ➤ TIME - ${data.timestamp}
+
+|➤ AGO - ${data.ago}
+
+ ©*BY BLAST-MD*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+  footer: tlang().footer,
+                headerType: 4,
+            };
+            return Void.sendMessage(citel.chat, buttonMessage, {
+                quoted: citel,
+	    }
+         let urlYt = text;
+            if(!text){ text=citel.quoted.text; }
+
+            if (!urlYt.startsWith("http")) 
+            {
+                let yts = require("secktor-pack");
+                let search = await yts(text);
+                let anu = search.videos[0];
+                urlYt = anu.url; 
+            }
+            let infoYt = await ytdl.getInfo(urlYt);
+            if (infoYt.videoDetails.lengthSeconds >= 1200) return citel.reply(`*song not Found, Try Differ Name*`);
+            let titleYt = infoYt.videoDetails.title;   
+	    citel.reply(`_Downloading ${infoYt.videoDetails.title}?_`);
+            let randomName = getRandom(".mp3");
+            const stream = ytdl(urlYt, {
+                 filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128, })
+                 .pipe(fs.createWriteStream(`./${randomName}`));
+                
+	   await new Promise((resolve, reject) => { stream.on("error", reject);  stream.on("finish", resolve);  });
+            
+            let stats = fs.statSync(`./${randomName}`);
+            let fileSizeInBytes = stats.size;
+            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+            if (fileSizeInMegabytes <= dlsize) 
+            {
+                let yts = require("secktor-pack");
+                let search = await yts(text);
+                let buttonMessage = 
+				{
+				    audio: fs.readFileSync(`./${randomName}`),
+				    mimetype: 'audio/mpeg',
+				    fileName: titleYt + ".mp3",
+				    headerType: 4,
+				 }
+                 
+                await Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+                return fs.unlinkSync(`./${randomName}`);
+            } 
+            else {   citel.reply(`❌ File size bigger than 100mb.`);    }
+             return fs.unlinkSync(`./${randomName}`);
+   
+   }catch (e) { return citel.reply(`Error While Downloading Your Song`);  }
+})
+    
+//===========video-dl===========
+
+cmd({
+    pattern: "video",
+    desc: "download video",
+    category: "download",
+    react: "🎥",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*WAIT PLEASE...*")
+const search = await yts(q)
+const data = search.videos[0]
+const url = data.url
+
+let des = `*◆ GHOST-MD VIDEO DOWNLOADING.....◆*
+
+| ➤ TITLE - ${data.title}
+
+| ➤ VIEWS - ${data.views}
+
+| ➤ DESCRIPTION - ${data.description}
+
+| ➤ TIME - ${data.timestamp}
+
+| ➤ AGO - ${data.ago}
+
+*ᴘᴏᴡᴇʀᴅ ʙʏ ɢʜᴏꜱᴛ-ᴍᴅ*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:des},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)  
+let downloadUrl = down.dl_url
+
+//send video
+await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏꜱᴛ-ᴍᴅ"},{quoted:mek})
+    
+}catch(a){
+reply(`${a}`)
+}
+})
